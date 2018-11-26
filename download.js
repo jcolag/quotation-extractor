9386501d4db5ad9d57dc618dfd39bac89b0b7799
John Colagioia <john@colagioia.net>
2018-11-26 07:22:13 -0500

    Comment the download script

--- download.js
+++ download.js
@@ -4,11 +4,19 @@ const https = require('https');
 const urlPrefix = 'https://www.gutenberg.org/files/';
 const fileType = '.html';
 const books = [
+  // This could easily get away with just storing the book ID, as
+  // in "27889" for Bartlett's quotations, but since I'll need to
+  // reference the HTML files for each extraction process (the
+  // layouts are not remotely consistent, of course), knowing which
+  // is which is going to be helpful.
   { name: 'Familiar Quotations 9e John Bartlett', url: '27889/27889-h/27889-h.htm' },
 ];
 
 books.forEach(book => {
   https.get(`${urlPrefix}${book.url}`, (resp) => {
+    // For reference, this is specifically downloading the HTML
+    // editions of each book because those generally have enough
+    // semantic markup to make parsing easier.
     let page = '';
     resp.on('data', (next) => {
       page += next;
