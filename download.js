fe681a7fa4138fa1690050134bb12e2b61488a33
John Colagioia <john@colagioia.net>
2018-11-28 07:07:04 -0500

    Add additional candidates of possible use

--- download.js
+++ download.js
@@ -10,6 +10,14 @@ const books = [
   // layouts are not remotely consistent, of course), knowing which
   // is which is going to be helpful.
   { name: 'Familiar Quotations 9e John Bartlett', url: '27889/27889-h/27889-h.htm' },
+  { name: 'Dictionary of Quotations Rev James Wood', url: '48105/48105-h/48105-h.htm' },
+  { name: 'Familiar Quotations 1e John Bartlett', url: '16732/16732-h/16732-h.htm' },
+  { name: 'Book of Wise Sayings Clouston', url: '21130/21130-h/21130-h.htm' },
+  { name: 'Memorabilia Mathematica Moritz', url: '44730/44730-h/44730-h.htm' },
+  {
+    name: 'Handy Dictionary of Poetical Quotations George Powers',
+    url: '15119/15119-h/15119-h.htm'
+  },
 ];
 
 books.forEach(book => {
