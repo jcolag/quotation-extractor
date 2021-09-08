d31ca6d55df072d39651bdf34638d5371c62d03e
John Colagioia <john@colagioia.net>
2021-09-08 06:54:16 -0400

    Print a random quote

---
+++ go.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  console.log('Need a file prefix.');
+  process.exit(1);
+}
+
+const prefix = process.argv[2];
+const quotes = JSON.parse(fs.readFileSync(`${prefix}Quotes.json`));
+const authors = JSON.parse(fs.readFileSync(`${prefix}Authors.json`));
+
+const quote = quotes[Math.floor(Math.random() * quotes.length)];
+const author = authors.filter((a) => a.id === quote.author)[0];
+
+let size = quote.text.length + author.name.length + 1;
+
+console.log(quote.text);
+if (quote.text.indexOf('[') >= 0) {
+  const fn = quote.footnotes.join('\n');
+  console.log(fn);
+  size += fn.length + 1;
+}
+console.log(author.name);
+console.log(size);
+
